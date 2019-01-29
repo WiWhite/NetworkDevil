bacee8d315b91c15a6baf147d71d7a0f87e2ec06
Alexandr Slipchenko <bigsplash1990@gmail.com>
2019-01-29 18:07:41 +0200

    modern my_crontab

--- networkdev/my_crontab.py
+++ networkdev/my_crontab.py
@@ -1,30 +1,47 @@
 from getpass import getuser
 from crontab import CronTab
-import django
 import os
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'networkdev.settings')
-django.setup()
 
 
 def add_cron(minute, hour, day):
 
-    my_cron = CronTab(user=getuser())
+    try:
+        my_cron = CronTab(user=getuser())
 
-    job = my_cron.new(command='python3 backup_master.py; python3 limit_files.py')
-    job.minute.on(minute)
-    job.hour.on(hour)
-    job.dow.on(day)
+        job = my_cron.new(command='python3 backup_master.py; python3 limit_files.py')
+        job.minute.on(minute)
+        job.hour.on(hour)
+        job.dow.on(day)
 
-    my_cron.write()
+        my_cron.write()
+
+    except ValueError:
+        my_cron = CronTab(user=getuser())
+
+        job = my_cron.new(command='python3 backup_master.py; python3 limit_files.py')
+        job.minute.on(minute)
+        job.hour.on(hour)
+
+        my_cron.write()
 
 
 def update_cron(minute, hour, day):
-    existing_cron = CronTab(user=getuser())
-    for job in existing_cron:
-        if job.command == 'python3 backup_master.py; python3 limit_files.py':
-            job.minute.on(minute)
-            job.hour.on(hour)
-            job.dow.on(day)
+    
+    try:
+        existing_cron = CronTab(user=getuser())
+        for job in existing_cron:
+            if job.command == 'python3 backup_master.py; python3 limit_files.py':
+                job.minute.on(minute)
+                job.hour.on(hour)
+                job.dow.on(day)
 
-            existing_cron.write()
+                existing_cron.write()
+
+    except ValueError:
+        my_cron = CronTab(user=getuser())
+
+        job = my_cron.new(command='python3 backup_master.py; python3 limit_files.py')
+        job.minute.on(minute)
+        job.hour.on(hour)
+
+        my_cron.write()
