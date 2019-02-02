25236fc3fb88540957548e9869ec82a50e409f61
Alexandr Slipchenko <bigsplash1990@gmail.com>
2019-02-02 10:29:33 +0200

    modified diff_backups.py

--- networkdev/diff_backups.py
+++ networkdev/diff_backups.py
@@ -2,17 +2,16 @@ import os
 import difflib
 
 
-basedir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-maindir = os.path.join(basedir, 'BACKUPS')
-
-
 def files_for_diff(namedir):
-    
+
     files_list = os.listdir(namedir)
     full_list = [os.path.join(namedir, file) for file in files_list]
-    sorted_list = sorted(full_list, key=os.path.getmtime)
+    if len(full_list) < 2:
+        return full_list * 2
+    else:
+        sorted_list = sorted(full_list, key=os.path.getmtime)
+        return sorted_list[-2:]
 
-    return sorted_list[-2:]
 
 def difffiles(listfiles):
 
@@ -22,6 +21,7 @@ def difffiles(listfiles):
         d = difflib.Differ()
         diff = d.compare(file1split, file2split)
         return diff
+
 
 def show_diff(namedir):
 
