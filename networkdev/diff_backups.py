8930e2973fcc8f896cdba4658d2356059536178b
Alexandr Slipchenko <bigsplash1990@gmail.com>
2019-02-06 13:04:39 +0200

    modefied diff(add HttpDiff)

--- networkdev/diff_backups.py
+++ networkdev/diff_backups.py
@@ -1,33 +1,32 @@
 import os
 import difflib
+import sys
 
 
 def files_for_diff(namedir):
 
-    files_list = os.listdir(namedir)
-    full_list = [os.path.join(namedir, file) for file in files_list]
-    if len(full_list) < 2:
-        return full_list * 2
-    else:
-        sorted_list = sorted(full_list, key=os.path.getmtime)
-        return sorted_list[-2:]
+    try:
+        files_list = os.listdir(namedir)
+        full_list = [os.path.join(namedir, file) for file in files_list]
+        if len(full_list) < 2:
+            return full_list * 2
+        else:
+            sorted_list = sorted(full_list, key=os.path.getmtime)
+            return sorted_list[-2:]
+    except FileNotFoundError:
+        exit(1)
 
 
 def difffiles(listfiles):
 
     with open(listfiles[0], 'r') as file1, open(listfiles[1], 'r') as file2:
-        file1split = file1.read().splitlines()
-        file2split = file2.read().splitlines()
-        d = difflib.Differ()
-        diff = d.compare(file1split, file2split)
+        file1split = file1.readlines()
+        file2split = file2.readlines()
+        diff = difflib.HtmlDiff().make_file(file1split, file2split)
         return diff
 
 
 def show_diff(namedir):
 
-    diff_list = []
-
-    for line in difffiles(files_for_diff(namedir)):
-        diff_list.append(line)
-
-    return diff_list
+    table = difffiles(files_for_diff(namedir))
+    return table
